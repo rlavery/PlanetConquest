850a4bf513fe3e680db00fa00be4305d08c6f087
rlavery <robertl@uta.edu>
2013-12-01 17:08:24 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlanetConquest::Application.config.secret_key_base = '18b0d855d710307ee4f20f6ceb385874f5ac2cafc22ba0975db73bbc9cbff1e5fea5783caf77ebfde681a2a89c9ad1b4be5af1483b07e54034d27684e1562928'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PlanetConquest::Application.config.session_store :cookie_store, key: '_PlanetConquest_session'
